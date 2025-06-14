c21605271237f86341609aa6867f4beac7c83401
glhkang <glhkang@gmail.com>
2025-06-13 21:53:37 -0400

    correct loop for lottie player

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -61,7 +61,7 @@ export default function Home() {
                     background='transparent'
                     speed='1'
                     style={{ width: '200px', height: '200px' }}
-                    loop
+                    loop={'true'}
                     autoplay
                 ></dotlottie-player>
                 <button className='button' onClick={next}></button>
