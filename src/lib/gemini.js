43c89edae9e88a04875c9bab93ef84eaa1568f2a
glhkang <glhkang@gmail.com>
2025-06-15 15:47:58 -0400

    add try / catch when pinging gemini api

--- src/lib/gemini.js
+++ src/lib/gemini.js
@@ -1,20 +1,22 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
-console.log(
-    'zzzzzzzzzzzzzzzzzzzzzzzzzzAttempting to initialize Gemini with key:zzzzzzzzzzzzzzz:::::',
-    import.meta.env.VITE_GEMINI_API_KEY,
-);
 
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
 
 export async function askGemini(prompt) {
     const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
-    const result = await model.generateContent({
-        contents: [{ role: 'user', parts: [{ text: prompt }] }],
-        generationConfig: {
-            maxOutputTokens: 200,
-            temperature: 1.7,
-        },
-    });
-    const response = await result.response;
-    return response.text();
+
+    try {
+        const result = await model.generateContent({
+            contents: [{ role: 'user', parts: [{ text: prompt }] }],
+            generationConfig: {
+                maxOutputTokens: 200,
+                temperature: 1.7,
+            },
+        });
+        const response = await result.response;
+        return response.text();
+    } catch (err) {
+        console.error('Error calling Gemini API: ', err);
+        return 'Sorry, something went wrong with the AI service.';
+    }
 }
