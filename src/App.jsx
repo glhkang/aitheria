0c31a8b36595b505e42c4990884f30a3146f7783
glhkang <glhkang@gmail.com>
2025-06-15 14:50:12 -0400

    test without initial password

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import Home from './components/Home';
 import MoodChat from './components/MoodChat';
 import PasswordModal from './components/PasswordModal';
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 
 export default function App() {
-    const [isAuthed, setIsAuthed] = useState(false);
+    // const [isAuthed, setIsAuthed] = useState(false);
 
-    useEffect(() => {
-        const authed = sessionStorage.getItem('authenticated') === 'true';
-        setIsAuthed(authed);
-    }, []);
+    // useEffect(() => {
+    //     const authed = sessionStorage.getItem('authenticated') === 'true';
+    //     setIsAuthed(authed);
+    // }, []);
 
-    if (!isAuthed) {
-        return <PasswordModal onAuthSuccess={() => setIsAuthed(true)} />;
-    }
+    // if (!isAuthed) {
+    //     return <PasswordModal onAuthSuccess={() => setIsAuthed(true)} />;
+    // }
 
     return (
         <div
@@ -39,7 +39,7 @@ export default function App() {
                     <Route path='/chat/:characterId' element={<MoodChat />} />
                 </Routes>
             </main>
-            <Footer />
+            {/* <Footer /> */}
         </div>
     );
 }
